30507c25e69ce8875af8f7e33a3e446eb505ceea
Kobe De Broeck <kobe.debroeck1@gmail.com>
2024-12-03 14:10:39 +0100

    DocumentVectorizer: count number of documents in dir + add progress bar

--- search_engine/preprocessing/documentVectorizer.py
+++ search_engine/preprocessing/documentVectorizer.py
@@ -2,6 +2,12 @@ import numpy as np
 from sentence_transformers import SentenceTransformer
 from scipy import sparse
 import os
+from tqdm import tqdm
+
+def count_txt_files_in_folder(folder_path):
+    with os.scandir(folder_path) as entries:
+        txt_file_count = sum(1 for entry in entries if entry.is_file() and entry.name.endswith('.txt'))
+    return txt_file_count
 
 class DocumentVectorizer:
     def __init__(self, model: str, out_path: str):
@@ -10,7 +16,8 @@ class DocumentVectorizer:
         
     def compute_doc_matrix(self, directory: str) -> np.matrix:
         doc_vectors = []
-        for i in range(1, 1558):
+        document_count = count_txt_files_in_folder(directory)
+        for i in tqdm(range(1, document_count+1), desc="Reading documents..."):
             with open(f"{directory}/output_{i}.txt") as file:
                 text = file.read()
                 doc_vector = self.model.encode(text)
@@ -22,4 +29,3 @@ class DocumentVectorizer:
 if __name__ == "__main__":
     vectorizer = DocumentVectorizer("all-MiniLM-L6-v2", "doc_vectors")
     vectorizer.compute_doc_matrix("full_docs_small")
-
