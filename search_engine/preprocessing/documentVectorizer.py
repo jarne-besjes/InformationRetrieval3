049fa931c2b75b03acff8c737d61492c045ddb49
jarne-besjes <91670760+jarne-besjes@users.noreply.github.com>
2024-12-15 17:20:18 +0100

    Complete windowing

--- search_engine/preprocessing/documentVectorizer.py
+++ search_engine/preprocessing/documentVectorizer.py
@@ -10,10 +10,12 @@ from sklearn.decomposition import PCA
 import sys
 import shutil
 
+
 def count_txt_files_in_folder(folder_path):
     with os.scandir(folder_path) as entries:
         txt_file_count = sum(1 for entry in entries if entry.is_file() and entry.name.endswith('.txt'))
     return txt_file_count
+
 
 class DocumentVectorizer:
     def __init__(self, model: str, out_path: str):
@@ -30,12 +32,13 @@ class DocumentVectorizer:
                 windows.append(" ".join(window))
         return windows
 
-    def pool_embeddings(self, document_windows_embeddings, window_counts):
+    def pool_embeddings(self, document_windows_embeddings):
         return np.mean(document_windows_embeddings, axis=0)
+
     def compute_doc_matrix(self, directory: str, clustering: bool = False) -> np.matrix:
         document_count = count_txt_files_in_folder(directory)
         doc_embeddings = []
-        for i in tqdm(range(1, document_count+1), desc="Encoding documents..."):
+        for i in tqdm(range(1, document_count + 1), desc="Encoding documents..."):
             with open(f"{directory}/output_{i}.txt") as file:
                 text = file.read()
             windows = self.split_into_windows(text)
@@ -43,9 +46,8 @@ class DocumentVectorizer:
             for window in windows:
                 window = self.model.encode(window, convert_to_numpy=True)
                 embeddings.append(window)
-                doc_embedding = self.pool_embeddings(embeddings, [len(embeddings)])
+                doc_embedding = self.pool_embeddings(embeddings)
             doc_embeddings.append(doc_embedding)
-
 
         os.makedirs(self.out_path, exist_ok=True)
         doc_embeddings = np.array(doc_embeddings)
@@ -53,14 +55,14 @@ class DocumentVectorizer:
 
         if clustering:
             self.do_clustering(doc_embeddings, 10)
-    
+
     def do_clustering(self, document_embeddings, cluster_count: int):
         print("Clustering...")
         clustering_start_time = time.time()
         kmeans = KMeans(n_clusters=cluster_count, random_state=42)
         kmeans.fit(document_embeddings)
         cluster_assignments = kmeans.labels_
-        centroids = kmeans.cluster_centers_ 
+        centroids = kmeans.cluster_centers_
         centroids = centroids / np.linalg.norm(centroids, axis=1, keepdims=True)
         print(f"Done clustering. Elapsed: {(time.time() - clustering_start_time):.2f} seconds")
 
@@ -79,11 +81,16 @@ class DocumentVectorizer:
             np.save(f"{cluster_data_path}/cluster_{ci}.npy", cluster_vecs)
             np.save(f"{cluster_data_path}/cluster_doc_ids_{ci}.npy", cluster_doc_ids)
 
+
 import argparse
+
 argparser = argparse.ArgumentParser(description='Document Vectorizer')
-argparser.add_argument('--mode', type=str, help='Select mode: cluster(=cluster existing embeddings file)', required=True)
-argparser.add_argument('--index-folder', type=str, help='The location of the folder where document embeddings file is stored', required=True)
-argparser.add_argument('--cluster-count', type=int, default=10, required=False, help='Create cluster-count clusters for the *existing* document_embeddings.npy file stored in index-folder')
+argparser.add_argument('--mode', type=str, help='Select mode: cluster(=cluster existing embeddings file)',
+                       required=True)
+argparser.add_argument('--index-folder', type=str,
+                       help='The location of the folder where document embeddings file is stored', required=True)
+argparser.add_argument('--cluster-count', type=int, default=10, required=False,
+                       help='Create cluster-count clusters for the *existing* document_embeddings.npy file stored in index-folder')
 
 if __name__ == "__main__":
     try:
@@ -101,7 +108,8 @@ if __name__ == "__main__":
         try:
             document_embeddings = np.load(f"{index_folder}/document_embeddings.npy")
         except FileNotFoundError:
-            print(f"Error: The file '{index_folder}/document_embeddings.npy' does not exist. Please check the path and try again.")
+            print(
+                f"Error: The file '{index_folder}/document_embeddings.npy' does not exist. Please check the path and try again.")
             sys.exit(1)
         except Exception as e:
             print(f"An error occurred reading the document embeddings npy file: {e}")
